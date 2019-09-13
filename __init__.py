5d853c601bad4f54f2bacf0b231b60a366f7cb0a
Raimon Esteve <raimon@nan-tic.com>
2019-09-13 16:12:23 +0200

    Upgrade py3

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 #This file is part redsysgateway blueprint for Flask.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-from redsysgateway import *
+from .redsysgateway import *
